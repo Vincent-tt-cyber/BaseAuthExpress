982f10ff4299fb19f45676f53981a1d7705b003e
Vincent_tt <evgeniy161102@gmail.com>
2024-04-19 19:31:32 +0300

    Сверстана шапка и навигация на регистрацию

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,19 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <div>Navbar</div>
+      <nav style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <img
+          style={{ marginRight: "20px" }}
+          width={140}
+            src="https://cdn-icons-png.flaticon.com/512/3211/3211343.png"
+            alt=""
+          />
+          <h4 style={{fontSize: 40}}>AUTH</h4>
+        </div>
+          <div>Регистрация</div>
+          <div>Логин</div>
+      </nav>
     </>
   );
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,9 +5,12 @@ import "./index.css";
 
 import { store } from "./store/store";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
